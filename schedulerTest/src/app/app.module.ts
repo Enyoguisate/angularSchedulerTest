5bfe71acc6250c3210d2cdae828ddaa356b60a37
Victor Martin <vmartin@volartec.aero>
2018-09-24 15:10:24 -0300

    added angular calendar

--- schedulerTest/src/app/app.module.ts
+++ schedulerTest/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NgCalendarModule  } from 'ionic2-calendar';
+import { EventModalModule } from '../pages/event-modal/event-modal.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgCalendarModule,
+    EventModalModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
